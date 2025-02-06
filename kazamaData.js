416c81eee4e65d171c69f47c6d8f8a554f58021a
Microintel <135629446+microintel@users.noreply.github.com>
2025-02-06 19:43:10 +0530

    Add files via upload

--- kazamaData.js
+++ kazamaData.js
@@ -1,12 +1,1 @@
-{
-     "name": "dani daniels", 
-     "phone": "123456",
-      "age": 35,
-      "gmail password":"nknchakli@123",
-      "home address":"london ksrtc bustand",
-      "aadhar address":"s/o dani daniels ,cpu street android badavane",
-      "gmail id":"dani@gmail.com",
-      "work":"actress, model",
-      "song":"https://youtu.be/yP9KiFTyBks?si=tbilMjFJEq6sBz4E",
-      "bank account number":"3727282828"
-};
+{"name": "dani daniels","phone": "123456","age": 35,"gmail password":"nknchakli@123","home address":"london ksrtc bustand","aadhar address":"s/o dani daniels ,cpu street android badavane","gmail id":"dani@gmail.com","work":"actress, model","song":"https://youtu.be/yP9KiFTyBks?si=tbilMjFJEq6sBz4E","bank account number":"3727282828"};
